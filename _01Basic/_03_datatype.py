a7dd339277b2eb96c4dc34ca809a3e8de5672c3c
balakoyyamani <balakoyyamani@gmail.com>
2026-07-04 00:38:28 +0530

    feat: add folder

---
+++ _01Basic/_03_datatype.py
@@ -0,0 +1,25 @@
+a=10
+b=10.10
+c=10+a
+d="Hello"   
+e=True
+f=[1,2,3,4,5]
+f[0]=11
+g=(5,2,3,4,5)
+h={1,2,3,4,5}
+i={
+    "name":"bala",
+    "age":21,
+    "city":"chennai"
+}
+
+print(type(a),"a:",a)
+print(type(b),"b:",b)
+print(type(c),"c:",c)
+print(type(d),"d:",d)
+print(type(e),"e:",e)
+print(type(f),"f:",f)
+print(type(g),"g:",g)
+print(type(h),"h:",h)
+print(type(i),"i:",i)
+
