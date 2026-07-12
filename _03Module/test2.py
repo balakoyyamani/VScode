4aa91678317dfe218ff2095876831fd8c81eea82
balakoyyamani <balakoyyamani@gmail.com>
2026-07-12 22:47:05 +0530

    feat: module added

---
+++ _03Module/test2.py
@@ -0,0 +1,16 @@
+import math
+from math import sqrt,factorial as fact
+print((int)(math.sqrt(25)))
+print(math.pi)
+print(math.factorial(5))
+print("fact : ",fact(5))
+
+import random
+
+print(random.randint(1,10))
+a=["bala","Gnanam","siva","Kmani"]
+print(random.choice(a))
+
+import datetime
+
+print(datetime.datetime.now())
