e249579fba9f5e08938e10284d909e8d97ad3ba6
balakoyyamani <balakoyyamani@gmail.com>
2026-07-17 14:11:25 +0530

    fix: api_key from local

--- _09LLM_response/_01_app.py
+++ _09LLM_response/_01_app.py
@@ -1,12 +1,16 @@
 from google import genai
-
-client=genai.Client(api_key="")
-
+import os
+print(os.getenv("GOOGLE_API_KEY"))
+client=genai.Client(api_key=os.getenv("GenAi_API_KEY"))
+cont=input("Enter the prompt :")
 response=client.models.generate_content(
     model="gemini-3.5-flash",
-    contents="Explain Python in one sentence."
+    contents=cont
 )
 print(response.text)
+
+#with open("response.json","w") as file:
+    #file.write(response.text)
 
 #for model in client.models.list():
 #    print(model.name)
