d798ec1a663571ebbec9bdc976ac2b3703d507be
balakoyyamani <balakoyyamani@gmail.com>
2026-07-29 00:00:42 +0530

    add: destructor file

---
+++ javascript/_07destructuring.js
@@ -0,0 +1,14 @@
+const employee={
+    id:1,
+    name:"bala",
+    city:"chennai"
+};
+
+const{id,name}=employee
+console.log(id)
+
+const array=["bala","Siva","Rama"]
+
+const[f,s]=array
+
+console.log(f)
